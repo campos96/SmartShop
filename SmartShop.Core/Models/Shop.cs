a6ae8ebcd9dbb593c7a07372f2e1ca3a862eaded
ccampos <cesarhcmartinez@gmail.com>
2023-05-19 01:48:12 -0700

    Set Shop Description and Logo nullable

--- SmartShop.Core/Models/Shop.cs
+++ SmartShop.Core/Models/Shop.cs
@@ -8,8 +8,8 @@
 
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public byte[] Logo { get; set; }
+        public byte[]? Logo { get; set; }
     }
 }
