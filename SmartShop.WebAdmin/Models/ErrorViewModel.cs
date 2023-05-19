d0956c583232ba20ad1251cabe39a0c5f5308f4c
ccampos <cesarhcmartinez@gmail.com>
2023-05-19 00:08:53 -0700

    Add WebAdmin project

---
+++ SmartShop.WebAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartShop.WebAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
