69974e733d5c0440c0fe6a52b9daf745dbff732b
ccampos <cesarhcmartinez@gmail.com>
2023-05-22 13:09:29 -0700

    Add react app

---
+++ smartshop.client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+import LayoutMain from './components/layouts/LayoutMain'
+
+function App() {
+  return (
+    <LayoutMain>
+      <h1>Hello worlk</h1>
+    </LayoutMain>
+  );
+}
+
+export default App;
