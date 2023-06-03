69cbdf0f5a41cb2134bf79d1ef025550eb07752e
ccampos <cesarhcmartinez@gmail.com>
2023-06-03 03:20:42 -0700

    BO: Add database migrations

--- SmartShop.WebAdmin/Migrations/20230603101758_AddUpdateForeignKeys.Designer.cs
+++ SmartShop.WebAdmin/Migrations/20230603101758_AddUpdateForeignKeys.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartShop.WebAdmin.Data;
 
@@ -11,9 +12,11 @@ using SmartShop.WebAdmin.Data;
 namespace SmartShop.WebAdmin.Migrations
 {
     [DbContext(typeof(SmartShopContext))]
-    partial class SmartShopContextModelSnapshot : ModelSnapshot
+    [Migration("20230603101758_AddUpdateForeignKeys")]
+    partial class AddUpdateForeignKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SmartShop.WebAdmin/Migrations/20230603101758_AddUpdateForeignKeys.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartShop.WebAdmin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdateForeignKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCategories_Shops_ShopId",
+                table: "ProductCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductConditions_Shops_ShopId",
+                table: "ProductConditions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductConditions_ProductConditionId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductDetails_ProductId",
+                table: "ProductDetails");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AccountId",
+                table: "Users",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductDetails_ProductId",
+                table: "ProductDetails",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCategories_Shops_ShopId",
+                table: "ProductCategories",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductConditions_Shops_ShopId",
+                table: "ProductConditions",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductConditions_ProductConditionId",
+                table: "Products",
+                column: "ProductConditionId",
+                principalTable: "ProductConditions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCategories_Shops_ShopId",
+                table: "ProductCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductConditions_Shops_ShopId",
+                table: "ProductConditions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductConditions_ProductConditionId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AccountId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductDetails_ProductId",
+                table: "ProductDetails");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductDetails_ProductId",
+                table: "ProductDetails",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCategories_Shops_ShopId",
+                table: "ProductCategories",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductConditions_Shops_ShopId",
+                table: "ProductConditions",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductConditions_ProductConditionId",
+                table: "Products",
+                column: "ProductConditionId",
+                principalTable: "ProductConditions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Shops_ShopId",
+                table: "Products",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id");
+        }
+    }
+}
